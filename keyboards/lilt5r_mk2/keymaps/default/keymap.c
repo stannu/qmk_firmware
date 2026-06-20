3b24c201dc50045cc266a1dcc6af680266bba926
stannu <kawabata@stannu.com>
2026-06-20 17:56:26 +0900

    20260620

--- keyboards/lilt5r_mk2/keymaps/default/keymap.c
+++ keyboards/lilt5r_mk2/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
     [0] = LAYOUT(
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_MUTE
+        KC_A, KC_B, KC_C, KC_D, KC_E, KC_G
     )
 };
 
